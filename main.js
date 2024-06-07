93fb652a4479dfff0d95ef8c359826198526ae04
hyojeong <okm0227@naver.com>
2024-06-07 11:20:26 +0900

    완료

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   function loadToDoList() {
     const storedList = JSON.parse(localStorage.getItem('todoList'));
-    //로컬스토리지에서 todolist에 해당하는 값 불러오기
+    //로컬스토리지에서 todolist에 해당하는 값 불러오기,JSON 문자열을 JavaScript 객체로 변환
     if (storedList) { //로컬스토리지에 항목이 저장되어있다면 조건문 실행
-      storedList.forEach(element => {
+      storedList.forEach(element => { // 저장된 각 항목에 대해 반복문을 실행
         const li = document.createElement("li");
-        const text = document.createTextNode(element.text);
-        li.appendChild(text);
+        const text = document.createTextNode(element.text); //텍스트 노드 생성
+        li.appendChild(text); // 생성된 텍스트 노드를 리스트 아이템 요소에 추가
         if (element.checked) {
           li.classList.toggle('checked');
         }
@@ -26,21 +26,22 @@ document.addEventListener('DOMContentLoaded', () => {
           saveToDoList(); // 로컬스토리지에 저장
         });
 
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("myUL").appendChild(li); //리스트 아이템을 ul 요소에 추가
       });
     }
   } 
 
   function saveToDoList () {
-    const todoListItems = document.querySelectorAll('#myUL li');
-    const todoList = [];
+    const todoListItems = document.querySelectorAll('#myUL li');   // ul 요소 내의 모든 li 요소
+    const todoList = [];     // 저장할 항목들을 담을 빈 배열을 생성
     todoListItems.forEach(item => {
       todoList.push({
         text: item.firstChild.textContent,
         checked: item.classList.contains('checked')
       });
-    });
+    }); //객체 형태로 배열에 추가
     localStorage.setItem('todoList', JSON.stringify(todoList));
+        // 배열을 JSON 문자열로 변환하여 로컬스토리지에 저장
   }
   
   function newElement() {
